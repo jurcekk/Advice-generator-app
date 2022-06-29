df2ceeefa369fc15396fddfa92defb8abe58a383
jurcekk <jurcekdavid@gmail.com>
2022-06-29 14:54:41 +0200

    Uradjen desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./style/index.scss";
+import divider from "./images/pattern-divider-desktop.svg";
+import icon from "./images/icon-dice.svg";
 import './App.css';
 
 function App() {
@@ -15,10 +17,20 @@ useEffect(()=> {
   } )
 }, [])
   return (
-    <Fragment>
-    <h1>{advice}</h1>
-     
-    </Fragment>
+    <div className="container">
+       {/* <h1>{advice}</h1> */}
+     <div className="card">
+      <p className='id'>ADVICE #{advice[1]}</p>
+      <p className='advice'>
+        {advice[0]}
+      </p>
+      <img src={divider} alt="Divider" className='divider'/><br />
+      <div className='iconContainer'>
+      <img src={icon} alt="Icon" className='icon'/>
+      </div>
+     </div>
+     </div>
+   
   );
 }
 

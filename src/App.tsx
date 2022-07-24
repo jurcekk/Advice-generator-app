e951a9345fe49a8ad3f174a486e8b54d89119bb7
jurcekk <jurcekdavid@gmail.com>
2022-07-24 15:03:36 +0200

    style: code formatted

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import "./style/index.scss";
+import { useEffect, useState } from "react";
 import icon from "./images/icon-dice.svg";
-import './App.css';
+import "./style/index.scss";
 
 function App() {
-  const [advice, setAdvice] = useState([]);
-//https://api.adviceslip.com/advice
+  const [advice, setAdvice] = useState<string[]>([]);
 
-function Loading() {
-  fetch("https://api.adviceslip.com/advice").then(response => {
-    return response.json();
-  }).then(function(data){
-    let advice:any = [data.slip.advice, data.slip.id]
-    setAdvice(advice);
-  } )
-}
-useEffect(()=> {
-  Loading();
-}, [])
+  function Loading() {
+    fetch("https://api.adviceslip.com/advice")
+      .then((response) => {
+        return response.json();
+      })
+      .then(function (data) {
+        let advice: string[] = [data.slip.advice, data.slip.id];
+        setAdvice(advice);
+      });
+  }
+
+  useEffect(() => {
+    Loading();
+  }, []);
 
   return (
-    <div className="container">
-       {/* <h1>{advice}</h1> */}
-     <div className="card">
-      <p className='id'>ADVICE #{advice[1]}</p>
-      <p className='advice'>
-        {advice[0]}
-      </p>
-      <div className='dividerContainer'>
+    <div className='container'>
+      <div className='card'>
+        <p className='id'>ADVICE #{advice[1]}</p>
+        <p className='advice'>{advice[0]}</p>
+        <div className='dividerContainer'></div>
+        <div className='iconContainer' onClick={Loading}>
+          <img src={icon} alt='Icon' className='icon' />
+        </div>
       </div>
-      <div className='iconContainer' onClick={Loading}>
-      <img src={icon} alt="Icon" className='icon'/>
-      </div>
-     </div>
-     </div>
-   
+    </div>
   );
 }
-
 
 export default App;
